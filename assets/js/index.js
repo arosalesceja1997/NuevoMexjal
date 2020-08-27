f4235bcad6be0566fa5f02ba85e5ffb6ea81805c
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-08-27 13:39:37 -0500

    anthony - todo

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,9 @@
 //   $(this).fadeIn(500);
 // });
 
-function toggleHam() {
-  $(".me-top-menu").toggle();
-}
+// function toggleHam() {
+//   $(".me-top-menu").toggle();
+// }
 
 
 // $("a").mouseover(function (event) {
@@ -17,9 +17,21 @@ function toggleHam() {
 
 // });
 
-$(window).resize(function () {
-  //aqui el codigo que se ejecutara cuando se redimencione la ventana
-  var alto = $(window).height();
-  var ancho = $(window).width();
-  console.log(alto);
+// $(window).resize(function () {
+//   //aqui el codigo que se ejecutara cuando se redimencione la ventana
+//   var alto = $(window).height();
+//   var ancho = $(window).width();
+//   console.log(alto);
+// });
+$(document).ready(function () { //Hacia arriba
+  irArriba();
 });
+
+function irArriba() {
+  $('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 1000); });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 1000); });
+}
+
