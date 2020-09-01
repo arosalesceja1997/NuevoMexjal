afe7a0dd6943b44ac6b2fb3212ffdc315f9e40e4
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-08-31 23:49:54 -0500

    Nueva - logos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,3 @@
-// $(".me-flecha").hover(function () {
-//   $(this).fadeOut(100);
-//   $(this).fadeIn(500);
-// });
-
-// function toggleHam() {
-//   $(".me-top-menu").toggle();
-// }
-
-
-// $("a").mouseover(function (event) {
-//   $("#capa").addClass("clasecss");
-// });
-
-// $(document).ready(function () {
-//   //aquí meteremos las instrucciones que modifiquen el DOM
-
-// });
-
 $(window).resize(function () {
   //aqui el codigo que se ejecutara cuando se redimencione la ventana
   var alto = $(window).height();
